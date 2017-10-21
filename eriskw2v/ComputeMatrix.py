d3cb92fa04af21505d2169bc694858c0a19ef6b8
akshay kulkarni <kumar.akshay.kulkarni@gmail.com>
2017-10-21 16:38:28 -0500

    added bag methods

--- eriskw2v/ComputeMatrix.py
+++ eriskw2v/ComputeMatrix.py
@@ -3,14 +3,14 @@ import numpy as np
 
 
 class CompMatrix:
-        
-    def __init__(user_data,model):
-        self.user_data = user_data
-        self.model = model
-        #gensim.models.KeyedVectors.load_word2vec_format('C:\\Users\\abkma\\anlp\\GoogleNews-vectors-negative300.bin', binary=True)
+		
+	def __init__(model):
+		self.model = model
+		#gensim.models.KeyedVectors.load_word2vec_format('C:\\Users\\abkma\\anlp\\GoogleNews-vectors-negative300.bin', binary=True)
  
-    def get_matrix():
-        pass
+	def get_matrix(bag):
+		#return [[self.model.word_vec(word,use_norm=True)] for word in bag]
+		pass
 
-    def get_concat_matrix(matrix1,matrix2):
-        pass
+	def get_concat_matrix(bag1,bag2):
+		pass
--- eriskw2v/GetUserDict.py
+++ eriskw2v/GetUserDict.py
@@ -9,14 +9,27 @@ class UserDict:
 
 	def __init__(self,path):
 		self.path = path
+		self.files = (f for f in os.listdir(self.path) if isfile(join(self.path,f)))
 
 	def get_user_data(self):
-		files = (f for f in os.listdir(self.path) if isfile(join(self.path,f)))
 		user_data = {}
-		for f in files:
+		for f in self.files:
 			with open(join(self.path,f),errors='ignore') as read:
 				all_words = word_tokenize(''.join(line for line in read ))
 				all_words = [ w.lower() for w in all_words]
 				user_data[f]=all_words
 		return user_data
+
+	def get_bag(self):
+		bag = set()
+		for f in self.files:
+			with open(join(self.path,f),errors='ignore') as read:
+				all_words = word_tokenize(''.join(line for line in read ))
+				for w in all_words:
+					bag.add(w.lower())
+		return list(bag) 
+		
+	def combine_bags(self,bags):
+		return set().union(*bags)
+
 
