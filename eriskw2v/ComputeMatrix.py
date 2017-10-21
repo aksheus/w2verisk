1e951025329307b84b5a3da216cdbafd7120bbe7
akshay kulkarni <kumar.akshay.kulkarni@gmail.com>
2017-10-21 15:51:07 -0500

    added compute matrix

--- eriskw2v/ComputeMatrix.py
+++ eriskw2v/ComputeMatrix.py
@@ -1,1 +1,16 @@
+import gensim.models
+import numpy as np
 
+
+class CompMatrix:
+        
+    def __init__(user_data,model):
+        self.user_data = user_data
+        self.model = model
+        #gensim.models.KeyedVectors.load_word2vec_format('C:\\Users\\abkma\\anlp\\GoogleNews-vectors-negative300.bin', binary=True)
+ 
+    def get_matrix():
+        pass
+
+    def get_concat_matrix(matrix1,matrix2):
+        pass
