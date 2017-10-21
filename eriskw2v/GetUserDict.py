2133628497325fc43278f4f75592ea7cd7d6f0f4
akshay kulkarni <kumar.akshay.kulkarni@gmail.com>
2017-10-21 16:43:02 -0500

    path inject via method

--- eriskw2v/GetUserDict.py
+++ eriskw2v/GetUserDict.py
@@ -7,23 +7,24 @@ isfile = lambda y: os.path.isfile(y)
 
 class UserDict:
 
-	def __init__(self,path):
-		self.path = path
-		self.files = (f for f in os.listdir(self.path) if isfile(join(self.path,f)))
+	def __init__(self):
+		pass
 
-	def get_user_data(self):
+	def get_user_data(self,path):
 		user_data = {}
-		for f in self.files:
-			with open(join(self.path,f),errors='ignore') as read:
+		files = (f for f in os.listdir(path) if isfile(join(path,f)))
+		for f in files:
+			with open(join(path,f),errors='ignore') as read:
 				all_words = word_tokenize(''.join(line for line in read ))
 				all_words = [ w.lower() for w in all_words]
 				user_data[f]=all_words
 		return user_data
 
-	def get_bag(self):
+	def get_bag(self,path):
 		bag = set()
-		for f in self.files:
-			with open(join(self.path,f),errors='ignore') as read:
+		files = (f for f in os.listdir(path) if isfile(join(path,f)))
+		for f in files:
+			with open(join(path,f),errors='ignore') as read:
 				all_words = word_tokenize(''.join(line for line in read ))
 				for w in all_words:
 					bag.add(w.lower())
--- eriskw2v/eriskw2v.py
+++ eriskw2v/eriskw2v.py
@@ -1,6 +1,6 @@
 from GetUserDict import UserDict
 
 if __name__ == '__main__':
-    ud = UserDict('C:\\Users\\abkma\\nlp\\reddit-depression\\cleaned-train\\cleaned-pos\\chunk1-2-3-4-5-6-7-8-9-10')
-    d = ud.get_bag()
+    ud = UserDict()
+    d = ud.get_bag('C:\\Users\\abkma\\nlp\\reddit-depression\\cleaned-train\\cleaned-pos\\chunk1-2-3-4-5-6-7-8-9-10')
     print(d)
