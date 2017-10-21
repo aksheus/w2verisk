8438ef2e6a1fcfa538fe2da9b12c7b349aa6d195
akshay kulkarni <kumar.akshay.kulkarni@gmail.com>
2017-10-21 15:13:47 -0500

    added getuserdict

---
+++ eriskw2v/GetUserDict.py
@@ -0,0 +1,21 @@
+from nltk import word_tokenize
+import os
+
+join = lambda x,y: os.path.join(x,y)
+isdir = lambda x: os.path.isdir(x)
+isfile = lambda y: os.path.isfile(y)
+
+class UserDict:
+
+	def __init__(self,path):
+		self.path = path
+
+	def get_user_data(self):
+		files = (f for f in os.listdir(self.path) if isfile(join(self.path,f)))
+		user_data = {}
+		for f in files:
+			with open(join(self.path,f),errors='ignore') as read:
+				all_words = word_tokenize(''.join(line for line in read ))
+				user_data[f]=all_words
+		return user_data
+
